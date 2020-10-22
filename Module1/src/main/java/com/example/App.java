67afa76a63d2f0545f9f791780f2a2b71482fb30
zivy009 <371494097@qq.com>
2020-10-22 17:12:50 +0800

    test

--- Module1/src/main/java/com/example/App.java
+++ Module1/src/main/java/com/example/App.java
@@ -6,6 +6,7 @@ package com.example;
  */
 public class App 
 {
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
---
+++ Module1/src/main/java/com/example/Test.java
@@ -0,0 +1,7 @@
+package com.example;
+
+/**
+ * created by zivy on 2020/10/22.
+ */
+public class Test {
+}
